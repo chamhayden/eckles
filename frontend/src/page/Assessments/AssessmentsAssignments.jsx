e1678c4c9d171ed20115bd6247443d252dde60eb
Hayden Smith <hayden@pearler.com>
2022-10-21 20:09:10 +1100

    Ass3 update

--- frontend/src/page/Assessments/AssessmentsAssignments.jsx
+++ frontend/src/page/Assessments/AssessmentsAssignments.jsx
@@ -36,9 +36,9 @@ const AssessmentsAssignments = ({ }) => {
   return (
     <SubNavWrapper baseUrl={'/assessments/assignments'} menu={menu}>
       <>
-        <h2>{params.ass} has {['ass1', 'ass2'].includes(params.ass) ? '' : 'not '} been released!</h2>
-        {['ass1', 'ass2'].includes(params.ass) && (<Button variant="contained" size="large">
-          <a style={{ color: '#fff' }} target="_blank" href={params.ass === 'ass3' ? `https://docs.google.com/spreadsheets/d/1ePXvDAZ1ItjZcHtRV1N8Adt03jDrfLfcpS2oi9fA5eA/edit#gid=0` : `/~cs1531/redirect/?path=COMP6080/${getters.term}/students/_/${params.ass}/`}>
+        <h2>{params.ass} has {['ass1', 'ass2', 'ass3'].includes(params.ass) ? '' : 'not '} been released!</h2>
+        {['ass1', 'ass2', 'ass3'].includes(params.ass) && (<Button variant="contained" size="large">
+          <a style={{ color: '#fff' }} target="_blank" href={params.ass === 'ass3' ? `https://docs.google.com/spreadsheets/d/1E6uDWCg5osxfJ7KOs8qUDgEoJYqgAoJWBo3YL85tnoY/edit#gid=0` : `/~cs1531/redirect/?path=COMP6080/${getters.term}/students/_/${params.ass}/`}>
             View on gitlab
           </a>
         </Button>)}
