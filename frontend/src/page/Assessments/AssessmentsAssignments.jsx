83e02b01aba5f286d2117bfc8d9252802d19aa92
Hayden Smith <mail@chambalam.com>
2023-02-17 21:23:35 +1100

    Updating for ass2

--- frontend/src/page/Assessments/AssessmentsAssignments.jsx
+++ frontend/src/page/Assessments/AssessmentsAssignments.jsx
@@ -42,7 +42,7 @@ const AssessmentsAssignments = ({ }) => {
     <SubNavWrapper baseUrl={'/assessments/assignments'} menu={menu}>
       <>
         <h2>{params.ass} has {['ass1','ass2'].includes(params.ass) ? '' : 'not '} been released!</h2>
-        {['ass1'].includes(params.ass) && (<Button variant="contained" size="large">
+        {['ass1','ass2'].includes(params.ass) && (<Button variant="contained" size="large">
           <a style={{ color: '#fff' }} target="_blank" href={
             params.ass === 'ass1' ? `/~cs6080/redirect/?path=COMP6080/${getters.term}/students/_/pictocode/` :
             params.ass === 'ass2' ? `/~cs6080/redirect/?path=COMP6080/${getters.term}/students/_/funform/` :
