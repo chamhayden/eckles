8276f026ddad44250810e5da28a3678ce4956e35
Hayden Smith <mail@chambalam.com>
2023-02-19 18:18:46 +1100

    youtube playlist update

--- frontend/src/component/YoutubePlaylistButton.jsx
+++ frontend/src/component/YoutubePlaylistButton.jsx
@@ -4,7 +4,7 @@ const YoutubePlaylistButton = () => (
   <Button
     variant="contained"
     sx={{ margin: '0 auto 20px auto !important', display: 'block' }}
-      onClick={() => window.location.href = 'https://www.youtube.com/playlist?list=PLi2pCZz5m6GHzROMkaqo5HuMoa2GvOYf3'}
+      onClick={() => window.location.href = 'https://www.youtube.com/watch?v=kU1lGsUqqIE&list=PLi2pCZz5m6GH_-23-LKG7ZgfE5TbbFdQK&index=1'}
     >
       View all lectures on YouTube
   </Button>
