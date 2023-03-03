720274b5e52da30a9ae0b00f500e7d6670975553
Hayden Smith <hayden@pearler.com>
2023-03-03 12:46:03 +0900

    ass3 release push

--- frontend/src/page/Assessments/AssessmentsAssignments.jsx
+++ frontend/src/page/Assessments/AssessmentsAssignments.jsx
@@ -41,12 +41,12 @@ const AssessmentsAssignments = ({ }) => {
   return (
     <SubNavWrapper baseUrl={'/assessments/assignments'} menu={menu}>
       <>
-        <h2>{params.ass} has {['ass1','ass2'].includes(params.ass) ? '' : 'not '} been released!</h2>
-        {['ass1','ass2'].includes(params.ass) && (<Button variant="contained" size="large">
+        <h2>{params.ass} has {['ass1','ass2','ass3'].includes(params.ass) ? '' : 'not '} been released!</h2>
+        {['ass1','ass2','ass3'].includes(params.ass) && (<Button variant="contained" size="large">
           <a style={{ color: '#fff' }} target="_blank" href={
             params.ass === 'ass1' ? `/~cs6080/redirect/?path=COMP6080/${getters.term}/students/_/pictocode/` :
             params.ass === 'ass2' ? `/~cs6080/redirect/?path=COMP6080/${getters.term}/students/_/funform/` :
-            params.ass === 'ass3' ? `https://docs.google.com/spreadsheets/d/1E6uDWCg5osxfJ7KOs8qUDgEoJYqgAoJWBo3YL85tnoY/edit#gid=0`
+            params.ass === 'ass3' ? `https://docs.google.com/spreadsheets/d/1QZ1p5HSFyZmdfyvt7kyKpA_BfQdCDWs8T24o0S08nfQ/`
             : `/~cs6080/redirect/?path=COMP6080/${getters.term}/students/_/${params.ass}/`}>
             View on gitlab
           </a>
