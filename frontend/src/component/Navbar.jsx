4819b890c890c2a621724f9d7c7f3754242759b9
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-10-18 10:18:32 +1100

    update icon from img to svg component

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,39 +1,28 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import StarBorder from '@mui/icons-material/StarBorder';
-import { useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import ListItemButton from '@mui/material/ListItemButton';
 import Collapse from '@mui/material/Collapse';
-import { useLocation } from "react-router-dom"
 
 
-import { makeStyles } from "@material-ui/core/styles";
-
-import External from '../asset/external.png';
-import Logout from './Logout';
-import Header from './Header';
+import {makeStyles} from "@material-ui/core/styles";
 import mainlogo from '../asset/mainlogo.webp';
-import { isMobileWidth } from '../util/screen';
-import { Context, useContext } from '../context';
+import {Context, useContext} from '../context';
 
-import { getPrimaryNavList, getSecondaryNavList } from './NavList';
+import {getPrimaryNavList, getSecondaryNavList} from './NavList';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 const ExternalIcon = () => {
-  return <img style={{ width: '15px' }} src={External} />;
+  return <OpenInNewIcon/>;
 }
 
 const background = 'rgb(22,27,37)';
