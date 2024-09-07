41929eafa4da9b9932878389382743d8783cd528
Hayden Smith <haydensmith@MacBook-Pro-4.local>
2024-09-07 22:52:59 +1000

    Sample repo working again

--- backend/src/config.js
+++ backend/src/config.js
@@ -6,11 +6,11 @@ const config = {
   JWT_SECRET: 'TESTING2',
   PORT: 6080,
   COOKIE_EXPIRY: 60 * 60 * 24 * 7,
-  AIRTABLE_API_KEY: 'keyOJnoA6KHnByQUv',
   TERMS: {
     sample: {
       STUDENT_LIST_SH: 'echo "5555555\n3418003"',
-      AIRTABLE_BASE: 'appYoRmspUPWynt68',
+      AIRTABLE_BASE: 'app7CNZud3QDy1IP5',
+      AIRTABLE_API_KEY: 'patMWMSzpWIIw4ygQ.8905a12dae8ee6ecd484ea4dc3f1f2961f587b9c87a4682407063345298bd802',
       TUTOR_ID_LIST: [],
       AUDIT_ID_LIST: []
     },
