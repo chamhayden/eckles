30977f031ae5e0c6fd1dbf9adfe8d8c50ba4cc1b
Hayden Smith <haydensmith@MacBook-Pro-4.local>
2024-09-09 01:39:04 +1000

    A couple of aesthetic touches and integrating photopea

--- frontend/src/component/AssPrint.jsx
+++ frontend/src/component/AssPrint.jsx
@@ -10,7 +10,11 @@ const AssPrint = ({ mda, mdb, assNumber, startWeek }) => {
   const { getters } = useContext(Context);
   const readyFromWeek = (week) => {
     const thisWeek = getters.content.weeks.filter(w => w.week === week)[0];
-    return new Date() >= new Date(thisWeek.starts_on);
+
+    let readyDate = new Date(thisWeek.starts_on)
+    readyDate = new Date(readyDate.getTime() + readyDate.getTimezoneOffset() * 60000);
+
+    return new Date() >= readyDate;
   }
   return (
     <>
@@ -18,11 +22,15 @@ const AssPrint = ({ mda, mdb, assNumber, startWeek }) => {
       <Markdown remarkPlugins={[remarkGfm]}>{mda}</Markdown>
       <Markdown remarkPlugins={[remarkGfm]}>{`## 2. The Task 🔥🔥`}</Markdown>
       {readyFromWeek(assNumber) ? 
-        <a target="_blank" href={`${config.BASE_URL}/gitlabredir/${getters.term}/${`ass${assNumber}`}`}>Please see details about the task here.</a>
+        <>
+          <a target="_blank" href={`${config.BASE_URL}/gitlabredir/${getters.term}/${`ass${assNumber}`}`}>Please see details about the task here.</a>
+          <marquee>The reluctant use of a generally-advised-against &lt;marquee&gt; HTML tag is being used here just to get your attention so you know to click this link above.</marquee>
+        </>
       :
         <><span style={{ color: 'red', fontWeight: 'bold'}}>Details of this task are not yet available. Please check back on Monday of week {startWeek}.</span></>
       }    
       <Markdown remarkPlugins={[remarkGfm]}>{mdb}</Markdown>
+      <marquee>Another surprise &lt;marquee&gt; just to remind you that you're a cool cucumber 🥒 </marquee>
     </>
   );
 };
