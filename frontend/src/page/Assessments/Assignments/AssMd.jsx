d75ae4483f4a54ff042d08be61b643579063e4ee
Hayden Smith <haydensmith@MacBook-Pro-4.local>
2024-09-08 11:27:50 +1000

    Clean up the ass4 page

--- frontend/src/page/Assessments/Assignments/AssMd.jsx
+++ frontend/src/page/Assessments/Assignments/AssMd.jsx
@@ -516,21 +516,9 @@ export const ass4a = `
 
 ### 1.1. Background & Motivation
 
-In March of 2024 you and your friends pitched a startup idea to produce _An alternative to [slides.com](https://slides.com) that is a lean, lightweight app that is a lot more enjoyable and interesting to use_ and that will _revolutionise the presentations industry for decades to come_. You pitched this solution in the form of a web-based application, and called this quiz application 🪄🪄🪄**Presto**🪄🪄🪄.
+**This assignment is the process you building the front-end for that MVP(Minimum viable product) to the standards described.** Features need to be implemented in order for your React.js app to meet the requirements of the task, and to operate with the backend described in the spec.
 
-A week later you received a tentative $50,000 investment from an [Angel Investor](https://en.wikipedia.org/wiki/Angel_investor) pending you producing a working minimum viable product of the application.
-
-Shortly after you discussed the functionality and feature set with your friends, and wrote out a RESTful specification / interface together so that you can split up the frontend and backend work between the group. You build the frontend, they build the backend. To get things moving, the backend was built EXTREMELY light in order to reduce the amount of interfacing needed.
-
-Whilst you (and optionally another one of your friends) decided to work on building the frontend. You wrote a list of requirements and functionalities your frontend should adhere to (described in \`section 2\`). You also decided to complete this application in \`React.js\`, a declarative framework for building single page applications. This front-end will interact with a Restful API that your team members are producing, based on the pre-defined interface.
-
-Because your MVP is only going to be demonstrated once, your team considers it imperative that your frontend is thoroughly tested.
-
-To satisfy modern tastes and expectations you have also decided to ensure that the UI/UX and Accessibility standards are very high.
-
-To make it easier for your friends from non-coding backgrounds to demo this application, you have decided to deploy this application on [Vercel](https://vercel.com/).
-
-**This assignment is the process you building the front-end for that MVP(Minimum viable product) to the standards described.** This assignment is closely modelled off the popular website [slides](https://slides.com/). If you're not familiar with the site, we would recommend spending the time to try it out so that you can get a feel for how this application may function as a reference point.
+The requirements describe a series of **screens**. Screens can be presented as popups/modals, or full-page views. The term _Screen_ is used to refer to a specific state or representation of the web application. You have the flexibility to decide how to implement them, whether as modals, separate pages, or other appropriate UI components.
 
 ### 1.2. Lectures to watch
 
