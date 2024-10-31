e2ac8ffc37adeacecc4f6d48e21509227cfb6908
Sofia De Bellis <sofia.debellis23@gmail.com>
2024-10-31 23:02:09 +1100

    Fix filters

--- frontend/src/component/TutLecContentCard.jsx
+++ frontend/src/component/TutLecContentCard.jsx
@@ -66,7 +66,6 @@ export default function TutLecContentCard({
   const fullWeek = "Week " + week;
   const topic = topicEmoji + " " + topicName;
   const hash = hashCode(contentKey) % 400;
-
   return (
     <Card
       sx={{
