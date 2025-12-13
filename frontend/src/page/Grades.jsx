f1086bae92079c81aebcd51416b529a82133e0e9
Hayden Smith <mail@chambalam.com>
2025-12-13 18:25:07 +1100

    Grades page update

--- frontend/src/page/Grades.jsx
+++ frontend/src/page/Grades.jsx
@@ -50,16 +50,85 @@ const Grades = () => {
            <input type="text" value={zid} onChange={e => setZid(e.target.value)} placeholder="5555555" />
            <br />
            <button onClick={getGrades}>Search</button>
-           <br />
+         <br />
           </>
         }
         <br />
+        <h1>Main grades</h1>
         <table border="1" cellPadding="5">
           <tr>
             <th>Field</th>
             <th>Value</th>
           </tr>
-          {grades.map((value, key) => (
+          {grades.main.map((value, key) => (
+            <tr>
+              <td>{value[0]}</td>
+              {(value[1] === '.' || value[1] === '') ? (
+                <td><small><small><small>Unreleased</small></small></small></td>
+              ) : (
+                <td>{value[1].replace('{', '').replace('}', '')}{value[2] && ` / ${value[2]}`}</td>
+              )}
+            </tr>
+          ))}
+        </table>
+        <h1>Ass1 Breakdown</h1>
+        <table border="1" cellPadding="5">
+          <tr>
+            <th>Field</th>
+            <th>Value</th>
+          </tr>
+          {grades.ass1.map((value, key) => (
+            <tr>
+              <td>{value[0]}</td>
+              {(value[1] === '.' || value[1] === '') ? (
+                <td><small><small><small>Unreleased</small></small></small></td>
+              ) : (
+                <td>{value[1].replace('{', '').replace('}', '')}{value[2] && ` / ${value[2]}`}</td>
+              )}
+            </tr>
+          ))}
+        </table>
+        <h1>Ass2 Breakdown</h1>
+        <table border="1" cellPadding="5">
+          <tr>
+            <th>Field</th>
+            <th>Value</th>
+          </tr>
+          {grades.ass2.map((value, key) => (
+            <tr>
+              <td>{value[0]}</td>
+              {(value[1] === '.' || value[1] === '') ? (
+                <td><small><small><small>Unreleased</small></small></small></td>
+              ) : (
+                <td>{value[1].replace('{', '').replace('}', '')}{value[2] && ` / ${value[2]}`}</td>
+              )}
+            </tr>
+          ))}
+        </table>
+        <h1>Ass3 Breakdown</h1>
+        <table border="1" cellPadding="5">
+          <tr>
+            <th>Field</th>
+            <th>Value</th>
+          </tr>
+          {grades.ass3.map((value, key) => (
+            <tr>
+              <td>{value[0]}</td>
+              {(value[1] === '.' || value[1] === '') ? (
+                <td><small><small><small>Unreleased</small></small></small></td>
+              ) : (
+                <td>{value[1].replace('{', '').replace('}', '')}{value[2] && ` / ${value[2]}`}</td>
+              )}
+            </tr>
+          ))}
+        </table>
+        <h1>Ass4 Breakdown</h1>
+        <table border="1" cellPadding="5">
+          <tr>
+            <th>Field</th>
+            <th>Value</th>
+          </tr>
+          {grades.ass4.map((value, key) => (
             <tr>
               <td>{value[0]}</td>
               {(value[1] === '.' || value[1] === '') ? (
