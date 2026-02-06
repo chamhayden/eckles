70d76f5276caacc3174331c645dcf62c8c6e5013
ScarletZhang-z <yuanyuanzhang1210@gmail.com>
2026-02-06 12:55:18 +1100

    update help install

--- frontend/src/page/Help/HelpInstallation.jsx
+++ frontend/src/page/Help/HelpInstallation.jsx
@@ -5,6 +5,10 @@ import remarkGfm from 'remark-gfm';
 
 import HelpNodeSite from '../../asset/help-nodejs-site.png';
 import HelpRepl from '../../asset/help-repl.png';
+import HelpDownload from '../../asset/download.png';
+import HelpExe from '../../asset/exe.png';
+import HelpLocalMac from '../../asset/local-mac.png';
+import HelpCseLab from '../../asset/cselab.png';
 
 import helpInstallationMarkdown from './HelpInstallation.md';
 import makePage from '../../component/makePage';
@@ -12,11 +16,19 @@ import makePage from '../../component/makePage';
 const IMAGE_MAP = {
   'help-nodejs-site.png': HelpNodeSite,
   'help-repl.png': HelpRepl,
+  'download.png': HelpDownload,
+  'exe.png': HelpExe,
+  'local-mac.png': HelpLocalMac,
+  'cselab.png': HelpCseLab,
 };
 
 const IMAGE_STYLES = {
   'help-nodejs-site.png': { width: '80%', maxWidth: '600px', marginTop: '10px' },
   'help-repl.png': { width: '80%', maxWidth: '400px', marginTop: '10px' },
+  'download.png': { width: '80%', maxWidth: '700px', marginTop: '10px' },
+  'exe.png': { width: '80%', maxWidth: '700px', marginTop: '10px' },
+  'local-mac.png': { width: '80%', maxWidth: '700px', marginTop: '10px' },
+  'cselab.png': { width: '80%', maxWidth: '700px', marginTop: '10px' },
 };
 
 const getImageKey = (src = '') => src.split('/').pop();
